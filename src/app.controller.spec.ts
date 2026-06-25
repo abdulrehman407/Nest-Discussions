871d903cb4bc17cb2e1f0b54fd529ac58d318047
Abdul Rehman <hijaz2266@gmail.com>
2026-06-25 20:23:25 +0500

    Create app.controller.spec.ts for testing

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let appController: AppController;
+  let appService: AppService;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppService);
+  });
+
+  describe('root', () => {
+    it('should return a welcome message', () => {
+      expect(appController.getHello()).toBe('Welcome to Nest Discussions API!');
+    });
+  });
+});
